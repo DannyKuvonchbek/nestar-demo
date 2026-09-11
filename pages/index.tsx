7779c64494fee70d2546038b8e467e4fceffc7c6
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-09-11 22:28:00 +0900

    feat: MUI customized integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+import { Box, Container, Stack } from "@mui/material";
+import { brown, green } from "@mui/material/colors";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Nestar Demo</h1>
-      <p>Pages Router is working.</p>
-    </main>
+    <>
+      <Stack sx={{ background: "#81c784" }}>Header</Stack>
+      <Container>
+        <Stack flexDirection={"column"}>
+          <Box>Popular Properties</Box>
+          <Box>Top Agents</Box>
+          <Box>Top Properties</Box>
+          <Box>Events</Box>
+        </Stack>
+      </Container>
+      <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+    </>
   );
 }
