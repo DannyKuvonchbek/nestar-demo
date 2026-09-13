e0cdaaf3d1bad000e2ad475260182b72f61d7119
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-09-14 08:54:50 +0900

    fix: refactor pages components

--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -2,12 +2,12 @@ import withLayoutBasic from "../../libs/components/layout/layoutBasic";
 import { NextPage } from "next";
 import { Stack } from "@mui/material";
 
-const Community: NextPage = () => {
+const CS: NextPage = () => {
   return (
     <div style={{ margin: "20px 0px" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">CS</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
